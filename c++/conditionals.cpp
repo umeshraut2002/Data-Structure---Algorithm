0551230c00823baefb2643954deaff5a4c02e470
umesh raut <uraut6044@gmail.com>
2025-06-16 16:32:59 +0530

    conditionals in cpp

--- c++/conditionals.cpp
+++ c++/conditionals.cpp
@@ -1,1 +1,14 @@
-)
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 20;
+
+    if(age >= 18){
+        cout << "You can Drive" << endl;
+    }
+    else{
+        cout << "You can Not Drive" << endl;
+    }
+
+}
