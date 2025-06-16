b5aca341d6bdd67aee86f67978cfe4e9ca175fa0
umesh raut <uraut6044@gmail.com>
2025-06-16 17:05:50 +0530

    conditionals in cpp

--- c++/conditionals.cpp
+++ c++/conditionals.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main(){
-    int age = 20;
+    int age = 90;
 
-    if(age >= 18){
+    if(age >= 18 && age <= 85){
         cout << "You can Drive" << endl;
     }
-    else{
-        cout << "You can Not Drive" << endl;
+    else if(age >= 85){
+        cout << "You can not drive. You too old to drive" << endl;
     }
+    else{
+        cout << "You Cam Drive" << endl;
+    }
+
+    return 0;
 
 }
