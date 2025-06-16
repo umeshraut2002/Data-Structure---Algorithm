eba1a8c64eb2d776fcd4778a01e524b061ef903a
umesh raut <uraut6044@gmail.com>
2025-06-16 16:29:59 +0530

    input in cpp

---
+++ c++/input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1; 
+    int num2;
+
+    cout << "Enter the value of Num1: ";
+    cin >> num1;
+    cout << "You entered Num1 = " << num1 << endl;
+
+    cout << "Enter the value of Num2: ";
+    cin >> num2;
+    cout << "You entered Num2 = " << num2 << endl;
+
+    cout << "The sum of Num1 and Num2 is: " << num1 + num2;
+
+    return 0;
+}
