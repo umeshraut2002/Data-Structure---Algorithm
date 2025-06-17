4b3de3ae85a3e2cd35372e7b09a4bd874f9c430f
umesh raut <uraut6044@gmail.com>
2025-06-17 12:31:56 +0530

    for loop in cpp

--- c++/lops.cpp
+++ c++/lops.cpp
@@ -4,9 +4,16 @@ using namespace std;
 int main(){
     int count = 0;
 
-    while(count <= 10){
-        cout << count << endl;
-        count++;
+    // while loop 
+    // while(count <= 10){
+    //     cout << count << endl;
+    //     count++;
+    // }
+
+    // for loop
+
+    for(int i=0; i<=10; i++){
+        cout << i << endl;
     }
 
     return 0;
