db4a4022fd7150a7e506f1e06b9229ff1cba66a4
umesh raut <uraut6044@gmail.com>
2025-06-17 17:57:50 +0530

    functions in cpp

---
+++ c++/function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sumOfTwo(int a, int b){
+
+    int sum = a + b;
+
+    return sum;
+
+}
+
+int main(){
+
+    cout << "Sum = " << sumOfTwo(10, 4) << endl;
+
+    return 0;
+
+}
