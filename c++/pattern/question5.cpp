126f20224a1eee917df08d9df4757b3305539868
umesh raut <uraut6044@gmail.com>
2025-06-18 14:11:58 +0530

    triangle star pattern

---
+++ c++/pattern/question5.cpp
@@ -0,0 +1,32 @@
+//  1. Right-angled Triangle Star Pattern
+
+// Input: 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 5;
+
+    for(int i = 0; i < n; i++){
+
+        for(int j = 0; j < i + 1; j++){
+
+            cout << " * ";
+ 
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
