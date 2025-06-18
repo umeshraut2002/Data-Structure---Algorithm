ea369c83bf428f9fa48f99f1a75ab3de4fb60c82
umesh raut <uraut6044@gmail.com>
2025-06-18 13:48:14 +0530

    increase number pattern

---
+++ c++/pattern/Question3.cpp
@@ -0,0 +1,25 @@
+// 1 2 3 
+// 4 5 6
+// 7 8 9
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 3;
+    int num = 1;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+
+            cout << num << " ";
+            num++;
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
