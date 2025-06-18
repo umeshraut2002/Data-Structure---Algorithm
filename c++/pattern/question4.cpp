774c75eed5baa397f5cd7771396591672efae4cb
umesh raut <uraut6044@gmail.com>
2025-06-18 13:59:11 +0530

    character increase pattern in cpp

---
+++ c++/pattern/question4.cpp
@@ -0,0 +1,23 @@
+// A B C 
+// D E F 
+// G H I 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 3;
+    char ch = 'A';
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+
+            cout << ch << " ";
+            ch = ch + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
