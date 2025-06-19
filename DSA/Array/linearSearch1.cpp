ad8322472433b52e01297dc9e937aca56925c483
umesh raut <uraut6044@gmail.com>
2025-06-19 15:30:33 +0530

    linear search

---
+++ DSA/Array/linearSearch1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int linearSearchFun(int arr[], int size, int tar){
+
+    for(int i = 0; i < size; i++){
+        if(arr[i] == tar){
+            return i;
+        }
+    }
+
+    return -1;
+
+}
+
+int main(){
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size = 8;
+    int tar = 6;
+
+    cout << "Target Found At: " << linearSearchFun(arr, size, tar) << endl;
+
+    return 0;
+
+}
