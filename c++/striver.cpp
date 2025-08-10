28da3463b23482830280bd85a3d4a45d696070ca
umesh raut <uraut6044@gmail.com>
2025-08-10 10:14:18 +0530

    striver cpp

---
+++ c++/striver.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> // it includes all the libraries without seprately include.
+using namespace std;
+
+// int main(){
+//     int a = 29;
+//     int b = 3;
+//     int c = a + b;
+
+//     cout << c;
+
+//     return 0;
+// }
+
+int main(){
+    string str;
+    getline(cin, str);
+
+    cout << str;
+
+    return 0;
+}
