f65807aabacc11feb6a5b11cfa3e04c13a2ae2a2
umesh raut <uraut6044@gmail.com>
2025-08-11 11:42:26 +0530

    straight Star pattern

---
+++ patten/straight_star.cpp
@@ -0,0 +1,24 @@
+/* 
+ * * * *
+ * * * *
+ * * * *
+ * * * *
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int row = 4;
+    int col = 4;
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout << " * ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
