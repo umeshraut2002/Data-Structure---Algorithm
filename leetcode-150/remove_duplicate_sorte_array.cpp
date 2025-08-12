28a59de90bc07475b1860541e0215accff64c047
umesh raut <uraut6044@gmail.com>
2025-08-12 19:33:58 +0530

    duplicate from he sorted array

---
+++ leetcode-150/remove_duplicate_sorte_array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1, 1, 2};
+    int earr[];
+
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = arr.size(); j > i; j--){
+            if(arr[i] != arr[j]){
+                
+            }
+        }
+    }
+
+    return 0;
+}
