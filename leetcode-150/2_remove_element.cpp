1895eed55ee666b7ec89a5b1ea64a8e0b9bf02fc
umesh raut <uraut6044@gmail.com>
2025-08-12 12:44:20 +0530

    removelements

---
+++ leetcode-150/2_remove_element.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[] = {1,3,4,5,6};
+    int val = 5;
+
+    for(int i = 0; i < sizeof(arr); i++){
+        if(arr[i] == val){
+            
+        } 
+    }
+
+    return 0;
+}
