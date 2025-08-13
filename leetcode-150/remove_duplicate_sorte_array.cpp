cc908caa245bcb81973039cd1b825a7bb55d0dfa
umesh raut <uraut6044@gmail.com>
2025-08-13 10:47:39 +0530

    duplicate remove

--- leetcode-150/remove_duplicate_sorte_array.cpp
+++ leetcode-150/remove_duplicate_sorte_array.cpp
@@ -3,16 +3,23 @@ using namespace std;
 
 int main(){
 
-    int arr[] = {1, 1, 2};
-    int earr[];
+    int nums[] = {1,1,2};
+    int size = sizeof(nums);
+    int k = 0;
 
-    for(int i = 0; i < arr.size(); i++){
-        for(int j = arr.size(); j > i; j--){
-            if(arr[i] != arr[j]){
-                
-            }
+    for(int i = 0; i < size; i++){
+        if(nums[i] != nums[k]){
+            nums[++k] = nums[i];
+
+            cout << "---" << nums[++k] << endl;
         }
     }
 
+    cout<<k+1<<endl;
+
     return 0;
 }
+
+// [1,1,2,3,4,2,3,4,5]
+// output: [1,2,3,4,5]
+// k = 5
