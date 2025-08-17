0c41752a1565621cc10a503f44c9556d3932ff3a
umesh raut <uraut6044@gmail.com>
2025-08-17 09:06:13 +0530

    arrayelements  squares

---
+++ leetcode-150/sorted_Array_Square.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> nums = {-4,-1,0,3,10};
+
+    for(int i = 0; i < nums.size(); i++){
+        nums[i] = nums[i] * nums[i];
+    }
+
+    // cout << sort(nums.begin(), nums.end()) << endl;
+
+    return 0;
+}
