f42096e8897ae4c721ef1a21c352f995f713869c
umesh raut <uraut6044@gmail.com>
2025-08-20 19:44:30 +0530

    bubble sort algorithm optimized updated

--- algorithms/arrays_algo/sorting_algorithm/bubble_sort/bubbl_sort_algo.cpp
+++ algorithms/arrays_algo/sorting_algorithm/bubble_sort/bubbl_sort_algo.cpp
@@ -1,18 +1,35 @@
+/*
+buble sort iterates over the array always n-1 times
+time complexity is not much good TM : big O(n*n)
+sorting from non sorted elements ascending to descending 
+example below run the code and see the output:
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 void bubble_sort(int nums[], int n){
+    
     for(int i = 0; i < n - 1; i++){
+
+        bool isSwap = false; // if now swaps performed 
+
         for(int j = 0; j < n - i - 1; j++){
             
             if(nums[j] > nums[j+1]){
 
                 swap(nums[j], nums[j+1]);
+                isSwap = true; // for swaping true 
 
             }
-
         }
+
+        if(!isSwap){ // array is already sorted
+            return;
+        }
+
     }
+
 }
 
 void print_Array(int nums[], int n){
