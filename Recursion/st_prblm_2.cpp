471193f972df852cb0088e0f7b087f33f95aac96
umesh raut <uraut6044@gmail.com>
2025-08-25 12:01:25 +0530

    linearprint

---
+++ Recursion/st_prblm_2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void linearPrint(int i , int n){
+    if(){
+        return;
+    }
+
+    cout<< i << " ";
+
+    linearPrint(n-1, i);
+}
+
+int main(){
+
+    int n = 4;
+
+    linearPrint(1, n);
+
+    return 0;
+}
